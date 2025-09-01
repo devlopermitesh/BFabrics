419343e3563cb31a886d92077a23efd021c10452
unknown <gehlotmitesh63@gmail.com>
2025-09-01 13:38:24 +0530

    feat: initial setup done

---
+++ jest.config.ts
@@ -0,0 +1,24 @@
+import { Config } from 'jest'
+import nextJest from 'next/jest.js'
+ 
+const createJestConfig = nextJest({
+  dir: './',
+})
+ 
+const config: Config = {
+  coverageProvider: 'v8',
+  testEnvironment: 'jsdom',
+  collectCoverage: true, // ✅ ensures coverage is collected
+  coverageDirectory: 'coverage', // ✅ all reports will be stored here
+  coverageReporters: [
+    'text', // shows summary in terminal
+    'lcov', // for tools like Codecov
+    'html', // ✅ generates detailed HTML report
+  ],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+}
+ 
+export default createJestConfig(config)
